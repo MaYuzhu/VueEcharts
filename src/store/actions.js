83f2efb0a49a5c1e410716ffa991298cef81726c
MaYuzhu <mayuzhu123@sina.com>
2018-06-22 13:13:52 +0800

    接口 参数

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,12 @@ import {reqData} from '../tools'
 
 export default {
   async getData({commit},cb) {
-    const result = await reqData()
+    const result = await reqData({
+      buildcode:'3506I0006',
+      starttime:'2018-05-19+09%3A34%3A12',
+      endtime:'2018-06-20+09%3A34%3A12',
+      timetype:'day'
+    })
     commit('receiveData', {data01: result})
     cb && cb()
   }
--- src/tools/index.js
+++ src/tools/index.js
@@ -1,10 +1,18 @@
 import ajax from './ajax'
 
-export const reqData = () => ajax('api/zzcismp/tsd/getBuildDevsData.shtml?' +
+/*export const reqData = () => ajax('api/zzcismp/tsd/getBuildDevsData.shtml?' +
 
   'buildcode=3506I0006&' +
   'starttime=2018-06-19+09%3A34%3A12&' +
   'endtime=2018-06-20+09%3A34%3A12&' +
-  'timetype=minute')
+  'timetype=minute')*/
 
-//export const reqData = () => ajax('api/zzcismp/mntdesc/findTotalDescInfo.shtml?projcode=35060010&buildcode=3506I0008&devcode=3506I00080010F02')
+
+export const reqData =
+  ({buildcode,starttime,endtime,timetype}) => ajax('api/zzcismp/tsd/getBuildDevsData.shtml',
+                                            {buildcode,starttime,endtime,timetype})
+
+  // 'buildcode=3506I0006&' +
+  // 'starttime=2018-06-19+09%3A34%3A12&' +
+  // 'endtime=2018-06-20+09%3A34%3A12&' +
+  // 'timetype=minute'
