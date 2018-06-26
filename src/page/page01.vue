73b4a6b6fd6beb819aa2df49cd63cdcd384096f5
MaYuzhu <mayuzhu123@sina.com>
2018-06-26 15:27:52 +0800

    Y轴显示

--- src/page/page01.vue
+++ src/page/page01.vue
@@ -10,13 +10,16 @@
       <span slot="title">库容量</span>
       <span slot="text">abcd</span>
       <div slot="chart01"><div id="charts4"></div></div>
+
     </chart01>
+    <img src="./down.png" alt="">
   </div>
 
 </template>
 
 <script>
   import chart01 from '../components/chart01'
+
    //import '../assets/js/jquery-1.11.3'
   // import '../assets/js/config'
    //import '../assets/js/fun'
@@ -110,7 +113,7 @@
             data: option_xAxis
           },
           yAxis: [{
-            name: ('降雨量(单位：mm)'),
+            name: '降雨量(单位：mm)',
             axisLine: {
               show: false
             }
@@ -237,11 +240,11 @@
         // 指定的配置项和数据
         var option = {
           title: {
-            text: 'aaa',
+            text: '数据A',
             textStyle: {
-              color: 'red',
-              fontWeight: '500',
-              fontSize: '150px',
+              color: '#215710',
+              fontWeight: 500,
+              fontSize: 20,
             }
           },
           tooltip: {
@@ -253,35 +256,45 @@
                 type: "png",
                 name: "详细降雨量",
                 title: "下载",
-                icon: "image://../images/echarts/icon/down.png"
+                icon:"image://../static/down.png"
+                //icon:"'path://M30.9,53.2C16.8,53.2,5.3,41.7,5.3,27.6S16.8,2,30.9,2C45,2,56.4,13.5,56.4,27.6S45,53.2,30.9,53.2z M30.9,3.5C17.6,3.5,6.8,14.4,6.8,27.6c0,13.3,10.8,24.1,24.101,24.1C44.2,51.7,55,40.9,55,27.6C54.9,14.4,44.1,3.5,30.9,3.5z M36.9,35.8c0,0.601-0.4,1-0.9,1h-1.3c-0.5,0-0.9-0.399-0.9-1V19.5c0-0.6,0.4-1,0.9-1H36c0.5,0,0.9,0.4,0.9,1V35.8z M27.8,35.8 c0,0.601-0.4,1-0.9,1h-1.3c-0.5,0-0.9-0.399-0.9-1V19.5c0-0.6,0.4-1,0.9-1H27c0.5,0,0.9,0.4,0.9,1L27.8,35.8L27.8,35.8z'"
+                //icon: 'image://data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'
               }
             }
           },
           grid: {
             top: '80px',
-            left: '3%',
+            left: '5%',
             right: '8%',
             bottom: '13%',
             containLabel: true
           },
           legend: {
-            data: ['降雨量'],
+            //data: ['降雨量'],
+            data: [{
+              name: '降雨量',
+              // 设置文本为红色
+              textStyle: {
+                color: 'red'
+              }
+            }],
             bottom: '3px;'
           },
           xAxis: {
+
             data: option_xAxis
           },
           yAxis: [{
             name: ('降雨量(单位：mm)'),
             axisLine: {
-              show: false
+              show: true
             }
           }],
           series: [{
             type: 'line',
             //type: 'gauge',
             name: '降雨量',
-            barMinHeight: '1',
+            //barMinHeight: '1',
             itemStyle: {
               normal: {
                 color: new this.$echarts.graphic.LinearGradient(
